f93d6d35809c978a1b8497faa2ef0e059f74f473
ErikXu <81636759@qq.com>
2021-09-01 08:57:44 +0000

    Modify version.

--- pack.sh
+++ pack.sh
@@ -4,7 +4,7 @@ echo "SERVICE_NAME: "$SERVICE_NAME
 IMAGE_NAME=${IMAGE_NAME:-rust-ci}
 echo "IMAGE_NAME: "$IMAGE_NAME
 
-IMAGE_TAG=${IMAGE_TAG:-0.0.0}
+IMAGE_TAG=${IMAGE_TAG:-1.0.0}
 echo "IMAGE_TAG: "$IMAGE_TAG
 
 docker build --no-cache --disable-content-trust=true --build-arg RUST_FILE=$SERVICE_NAME -t $IMAGE_NAME:${IMAGE_TAG} -f ./docker/Dockerfile ./publish/release
